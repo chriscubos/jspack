39af2ae21d717087c8041079df8f35689097c35a
Ethan Sky Cubos <sky@philippineglobaloutsourcing.com>
2016-09-03 05:27:46 -0700

    updated to currently available packages 💋

--- src/config/jspack.php
+++ src/config/jspack.php
@@ -9,11 +9,7 @@ return [
 	'email'   => 'philippineoutsourcing@gmail.com',
 	'folder'  => 'packages/',
 	'package' => [
-		'bootstrap-slider' => ['bootstrap-slider.js', 'slider.css'],
-		'bootstrap-wysihtml5' => ['bootstrap3-wysihtml5.all.min.js', 'bootstrap3-wysihtml5.min.css'],
-		'chartjs' => ['Chart.min.js'],
-		'colorpicker' => ['bootstrap-colorpicker.min.js', 'bootstrap-colorpicker.min.css'],
-		'datatables' => ['dataTables.bootstrap.min.js', 'dataTables.bootstrap.css', 'jquery.dataTables.min.css', 'jquery.dataTables.min.js'],
-
+        'bootstrap' => ['bootstrap.css', 'bootstrap.js'],
+		'jquery' => ['jquery.js'],
 	]
 ];
